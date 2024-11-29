319378ddf09132ae8b35bab66415c58529a90596
Mencell <kuba@mencel.eu>
2024-11-29 11:44:10 +0100

    zadanie_1

---
+++ zadania/zadanie_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 11:39:10 2024
+
+@author: kuba
+"""
+
+#Napisz pętle, która zsumuje wartości od 1 do 10 
+
+suma = 0
+for i in range(1, 11):  # range(1, 11) generuje liczby od 1 do 10
+    suma += i
+
+print(suma)
